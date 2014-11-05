bf59263fcc8262a5850c6f985aaef0d93fc03476
Jethro <jethro.estrada@yahoo.com>
2014-11-05 14:19:34 -0800

    deploy minified

--- public/build/production.js
+++ public/build/production.js
@@ -27216,8 +27216,8 @@ angular.module("sportsStore")
     });
 
 angular.module('sportsStore')
-    .constant('domain','http://localhost:3000')
-//    .constant('domain','http://imba-app.herokuapp.com')
+//    .constant('domain','http://localhost:3000')
+    .constant('domain','http://imba-app.herokuapp.com')
     .constant('api','/some/api/info')
     .service('urls',function(domain,api, $routeParams){
         this.getDataUrl = function(username){
